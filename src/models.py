66f965075f1b7158e5132b81dd9f1701ac04e9f1
Douglas Martins <douglasmartins@MacBook-Pro-de-Douglas.local>
2019-11-09 11:06:25 -0300

    Add some models and make the fk relationship on the others models

--- src/models.py
+++ src/models.py
@@ -1,36 +1,30 @@
 from app import db
 
 
-# class Person(db.Model):
-#     __tablename__ = 'person'
-#
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String())
-#     contact = db.relationship('contact', backref='person', lazy=True)
-#     file = db.relationship('file', backref='person', lazy=True)
-#     created_at = db.Column(db.Date())
-#     updated_at = db.Column(db.Date())
-#     deleted_at = db.Column(db.Date())
-#
-#     def __init__(self, name, contact, file, created_at):
-#         self.name = name
-#         self.contact = contact
-#         self.file = file
-#         self.created_at = created_at
-#
-#     def __repr__(self):
-#         return '<id {}>'.format(self.id)
-#
-#     def serialize(self):
-#         return {
-#             'id': self.id,
-#             'name': self.name,
-#             'contact': self.contact,
-#             'file': self.file,
-#             'createdAt': self.created_at,
-#             'updatedAt': self.updated_at,
-#             'deletedAt': self.deleted_at
-#         }
+class File(db.Model):
+    __tablename__ = 'file'
+
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String)
+    created_at = db.Column(db.Date())
+    deleted_at = db.Column(db.Date())
+
+    person = db.relationship("Person", back_populates="file")
+
+    def __init__(self, url, created_at):
+        self.url = url
+        self.created_at = created_at
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'url': self.url,
+            'created_at': self.created_at,
+            'deleted_at': self.deleted_at
+        }
 
 
 class Contact(db.Model):
@@ -42,6 +36,8 @@ class Contact(db.Model):
     created_at = db.Column(db.Date())
     updated_at = db.Column(db.Date())
     deleted_at = db.Column(db.Date())
+
+    person = db.relationship("Person", back_populates="contact")
 
     def __init__(self, telephone, email, created_at):
         self.telephone = telephone
@@ -59,4 +55,41 @@ class Contact(db.Model):
             'createdAt': self.created_at,
             'updatedAt': self.updated_at,
             'deletedAt': self.deleted_at,
+        }
+
+
+class Person(db.Model):
+    __tablename__ = 'person'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+
+    contact_id = db.Column(db.Integer, db.ForeignKey('contact.id'))
+    contact = db.relationship("Contact", back_populates="person")
+
+    file_id = db.Column(db.Integer, db.ForeignKey('file.id'))
+    file = db.relationship("File", back_populates="person")
+
+    created_at = db.Column(db.Date())
+    updated_at = db.Column(db.Date())
+    deleted_at = db.Column(db.Date())
+
+    def __init__(self, name, contact, file, created_at):
+        self.name = name
+        self.contact = contact
+        self.file = file
+        self.created_at = created_at
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'contact': self.contact,
+            'file': self.file,
+            'createdAt': self.created_at,
+            'updatedAt': self.updated_at,
+            'deletedAt': self.deleted_at
         }
